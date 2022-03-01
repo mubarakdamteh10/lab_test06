4a706d6172506cbe2c61af83c4d388a088682232
mubarakdamteh10 <originth115@gmail.com>
2022-03-01 10:37:31 +0700

    test another function

--- routes/year_route.py
+++ routes/year_route.py
@@ -7,7 +7,13 @@ year_api_router = APIRouter()
 @year_api_router.get("/service/getage")
 async def getyear_test(year : int):
     cerrent_year = 2565
-    age = cerrent_year - year
+    if year != 0 :
+        if year > cerrent_year:
+            age = "Value out of Bound"
+        else:
+            age = cerrent_year - year
+    elif year <= 0 :
+        age = "Value = 0 or less than"
+        
     return {"age": age}
-    
-    
+
--- tests/test_years.py
+++ tests/test_years.py
@@ -1,3 +1,5 @@
+from pydoc import cli
+from urllib import response
 from fastapi.testclient import TestClient
 import sys        
 sys.path.insert(0, '../lab_test06')        
@@ -5,9 +7,23 @@ from main import app
 
 client = TestClient(app)
     
-def test_yeat_data_api():
+def test_year_data_api():
     output = 25
     input = "2540"
     response = client.get("/service/getage?year="+input)
     assert response.status_code == 200
     assert response.json() == {"age": output}
+
+def test_year_Zero():
+    output = "Value = 0 or less than"
+    input = "0"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() =={"age":output}
+    
+def test_year_Out_of_bound():
+    output = "Value out of Bound"
+    input = "2566"
+    response = client.get("/service/getage?year="+input)
+    assert response.status_code == 200
+    assert response.json() =={"age":output}
